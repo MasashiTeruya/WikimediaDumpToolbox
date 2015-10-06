b9133906d8d481e74f5cf421b76f3c9e70b017eb
Masashi Teruya <MasashiTeruya@outlook.com>
2015-10-07 00:50:17 +0900

    Update assembly info

--- src/WikimediaDumpToolbox.SampleApp/Properties/AssemblyInfo.cs
+++ src/WikimediaDumpToolbox.SampleApp/Properties/AssemblyInfo.cs
@@ -2,35 +2,14 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
-// アセンブリに関連付けられている情報を変更するには、
-// これらの属性値を変更してください。
 [assembly: AssemblyTitle("WikimediaDumpToolbox.SampleApp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sample application for WikimediaDumpToolbox")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Masashi Teruya")]
 [assembly: AssemblyProduct("WikimediaDumpToolbox.SampleApp")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Masashi Teruya 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
-// 参照不可能になります。COM からこのアセンブリ内の型にアクセスする場合は、
-// その型の ComVisible 属性を true に設定してください。
 [assembly: ComVisible(false)]
-
-// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
 [assembly: Guid("5d815535-5bf9-467a-94b4-88d64f0038b0")]
-
-// アセンブリのバージョン情報は次の 4 つの値で構成されています:
-//
-//      メジャー バージョン
-//      マイナー バージョン
-//      ビルド番号
-//      Revision
-//
-// すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
-// 既定値にすることができます:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
